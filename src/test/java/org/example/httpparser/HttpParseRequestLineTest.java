50d2a4055ddc3e0d2111f99d77d8ab6a7e99e7b0
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-02-10 13:42:21 +0100

    added tests for throwing error

---
+++ src/test/java/org/example/httpparser/HttpParseRequestLineTest.java
@@ -0,0 +1,56 @@
+package org.example.httpparser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpParseRequestLineTest {
+    private HttpParseRequestLine httpParseRequestLine;
+
+    @BeforeEach
+    void setUp() {
+        httpParseRequestLine = new HttpParseRequestLine();
+    }
+
+    @Test
+    void testParserWithTestRequestLine() throws IOException {
+        String testString = "GET / HTTP/1.1";
+
+        InputStream in = new ByteArrayInputStream(testString.getBytes());
+
+        httpParseRequestLine.parseHttpRequest(in);
+
+        assertThat(httpParseRequestLine.getMethod()).isEqualTo("GET");
+        assertThat(httpParseRequestLine.getUri()).isEqualTo("/");
+        assertThat(httpParseRequestLine.getVersion()).isEqualTo("HTTP/1.1");
+    }
+
+
+    @Test
+    void testParserThrowErrorWhenNull() throws IOException {
+        InputStream in = new ByteArrayInputStream("".getBytes());
+        Exception exception = assertThrows(
+                IOException.class, () -> httpParseRequestLine.parseHttpRequest(in)
+        );
+
+        assertThat(exception.getMessage()).isEqualTo("HTTP Request Line is Empty");
+    }
+
+    @Test
+    void testParserThrowErrorWhenMethodIsInvalid() throws IOException {
+        String testString = "get / HTTP/1.1";
+        InputStream in = new ByteArrayInputStream(testString.getBytes());
+
+        Exception exception = assertThrows(
+                IOException.class, () -> httpParseRequestLine.parseHttpRequest(in)
+        );
+        assertThat(exception.getMessage()).isEqualTo("Invalid HTTP method");
+    }
+
+}
