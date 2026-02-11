85cdff71b763cb61a6e2382639a6ea994ca1d22f
Eric Phu <eraiicphu@gmail.com>
2026-02-11 21:54:57 +0100

    added interfaces Filter and FilterChain with Java Servlet Filter architecture.

--- src/main/java/org/example/filter/Filter.java
+++ src/main/java/org/example/filter/Filter.java
@@ -1,4 +1,14 @@
 package org.example.filter;
 
+import org.example.http.HttpResponseBuilder;
+
+import java.net.http.HttpRequest;
+
 public interface Filter {
+    void init();
+
+    void doFilter(HttpRequest request, HttpResponseBuilder response, FilterChain chain);
+
+    void destroy();
+
 }
--- src/main/java/org/example/filter/FilterChain.java
+++ src/main/java/org/example/filter/FilterChain.java
@@ -1,4 +1,10 @@
 package org.example.filter;
 
+import org.example.http.HttpResponseBuilder;
+
+import java.net.http.HttpRequest;
+
 public interface FilterChain {
+
+    void doFilter(HttpRequest request, HttpResponseBuilder response, FilterChain chain);
 }
