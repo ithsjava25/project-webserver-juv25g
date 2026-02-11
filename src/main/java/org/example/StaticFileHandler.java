dc0124c6f575c4fb7908bdaeddb58bcac4d9682f
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-02-11 18:50:10 +0100

    Add support for serving static files
    - Introduced `StaticFileHandler` to serve static files from the `www` directory.
    - Enhanced `HttpResponseBuilder` to support byte array bodies.
    - Made `setReader` method public in `HttpParser` to improve flexibility.
    - Code tested via Insomnia by coding a temporary method for multithreads in TcpServer.java, this is now removed before future implementations of ConnectionHandler
    
    Co-authored-by: Daniel Fahlén xsz300@gmail.com

---
+++ src/main/java/org/example/StaticFileHandler.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.example.http.HttpResponseBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.util.Map;
+
+public class StaticFileHandler {
+    private static final String WEB_ROOT = "www";
+    private byte[] fileBytes;
+
+    public StaticFileHandler(){}
+
+    public void handleGetRequest(String uri) throws IOException {
+
+        File file = new File(WEB_ROOT, uri);
+        fileBytes = Files.readAllBytes(file.toPath());
+    }
+
+    public void sendGetRequest(OutputStream outputStream, String uri) throws IOException{
+        handleGetRequest(uri);
+        HttpResponseBuilder response = new HttpResponseBuilder();
+        response.setHeaders(Map.of("Content-Type", "text/html; charset=utf-8"));
+        response.setBody(fileBytes);
+        PrintWriter writer = new PrintWriter(outputStream, true);
+        writer.println(response.build());
+
+    }
+
+}
