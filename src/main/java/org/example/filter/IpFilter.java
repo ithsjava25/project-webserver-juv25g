a1e5c681dfde055043aa25284962a37966d9345e
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-02-13 11:30:10 +0100

    Add IpFilter and corresponding test skeleton
    Co-authored-by: Rickard Ankar <rickard.ankar@iths.se>

---
+++ src/main/java/org/example/filter/IpFilter.java
@@ -0,0 +1,22 @@
+package org.example.filter;
+
+import org.example.http.HttpResponseBuilder;
+import org.example.httpparser.HttpRequest;
+
+public class IpFilter implements Filter {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void doFilter(HttpRequest request, HttpResponseBuilder response, FilterChain chain) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
