37bb7deda4ab8b4a1acbaba083b9d1ebf77db1ab
Anna Ziafar <annaaziafar@gmail.com>
2026-02-22 18:37:20 +0100

    Add LoggFilter and update HttpResponseBuilder with method getStatusCode() in order to use it in logging

--- src/main/java/org/example/filter/LoggingFilter.java
+++ src/main/java/org/example/filter/LoggingFilter.java
@@ -20,14 +20,11 @@ public class LoggingFilter implements Filter {
 
         try {
             chain.doFilter(request, response);
-        } catch (Exception e) {
-            if(response.getStatusCode() == HttpResponseBuilder.SC_OK)
-                response.setStatusCode(HttpResponseBuilder.SC_INTERNAL_SERVER_ERROR);
         } finally {
             long endTime = System.nanoTime();
             long processingTimeInMs = (endTime - startTime) / 1000000;
 
-            String message = String.format("REQUEST: %s %s | STATUS: %s | TIME: %dms",
+            String message = String.format("REQUEST: %s%s | STATUS: %s | TIME: %dms",
                     request.getMethod(), request.getPath(), response.getStatusCode(), processingTimeInMs);
 
             logg.info(message);
